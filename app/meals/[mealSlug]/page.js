3c0cb9cb8e5d7fe0bffab09d3a7e83982d5f2f74
Dusica Krstic <pitchweiss.dev@gmail.com>
2023-12-28 19:13:04 +0100

    Showing individual meals

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,33 @@
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMealBySlug } from "@/lib/meals";
+
 export default function MealPage({ params }) {
   const { mealSlug } = params;
+
+  const meal = getMealBySlug(mealSlug);
   return (
-    <main>
-      <p>{mealSlug}</p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill alt={meal.title} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions.replace(/\n/g, "<br />"),
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
